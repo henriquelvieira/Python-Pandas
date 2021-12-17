3f6631f97362bac39a17d741a0349f1be4126524
Henrique Lopes <henrique.l.vieira92@gmail.com>
2021-12-17 08:57:05 -0300

    Add files via upload

--- data_frames.py
+++ data_frames.py
@@ -1,17 +1,31 @@
 import pandas as pd
+from matplotlib import pyplot as plt
 
 vCaminhoTabela = r"data\indexdata.csv"
 dados = pd.read_csv(vCaminhoTabela)
 
 #CRIAR UM DF APENAS COM AS COLUNAS ESPECIFICADAS
 df_cotacao = dados[['Index','Date','Close']] 
-print(df_cotacao)
+# print(df_cotacao)
 
 #CRIAR UM DF APENAS COM OS REGISTROS ONDE O CAMPO INDEX SEJA DIFERENTE DE NYA
 df_cotacao_remover = df_cotacao.loc[(df_cotacao['Index'] != 'NYA')] 
-print(df_cotacao_remover)
+# print(df_cotacao_remover)
 
 #CRIAR UM DF APENAS COM OS REGISTRO QUE NÃO ESTEJAM NO DF df_cotacao_remover 
-df_cotacao_final = df_cotacao.drop(df_cotacao_remover.index)
+df_cotacao_filtrado = df_cotacao.drop(df_cotacao_remover.index)
+# print(df_cotacao_filtrado)
+
+#CRIAR DV FILTRANDO OS REGISTROS PELO INTERVALO DO INDICE
+df_cotacao_final = df_cotacao_filtrado[13900:]
+# df_cotacao_final = df_cotacao_filtrado[13900:13904] 
 print(df_cotacao_final)
 
+#GERAÇÃO DE UM GRÁFICO COM BASE NOS DADOS DO DF
+plt.figure(figsize=(15,4))
+plt.style.use('ggplot')
+plt.title('Cotação histórica da Bolsa NYA')
+plt.plot(df_cotacao_final['Date'], df_cotacao_final['Close'], ls='--')
+plt.legend(df_cotacao_final['Index'], loc=2)
+plt.xticks(['2021-03-23', '2021-04-23', '2021-05-28'])
+plt.show()
